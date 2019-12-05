8a1d1f901582cfe784a8d94ed03ae1920c0a79b5
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-12-05 14:52:23 +0000

    start theming

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<nav className="nav">
+			<button className="btn btn--white">Button</button>
+		</nav>
+	);
+};
+
+export default Navbar;
