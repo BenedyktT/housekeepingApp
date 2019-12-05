68794bef4f09294b55ae666a1d4450cc8bfd624f
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-12-05 10:49:52 +0000

    init client

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
 const otplib = require("otplib");
 const atob = require("atob");
 const secret = require("config").get("secret");
+const axios = require("axios");
 
 otplib.totp.options = {
 	digits: 8,
 	algorithm: "sha256",
 	encoding: "hex"
 };
+const token = otplib.totp.generate(atob(secret));
 
 module.exports = token;
--- routes/api/cleanstatus.js
+++ routes/api/cleanstatus.js
@@ -2,17 +2,22 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const token = require("../../config/config");
-axios.defaults.baseURL = "https://api.roomercloud.net:8080";
+var xmldoc = require("xmldoc");
+
+axios.defaults.baseURL = "https://api.roomercloud.net";
 axios.defaults.headers.common["Promoir-Roomer-Hotel-Secret"] = token;
 axios.defaults.headers.common["Promoir-Roomer-Hotel-ApplicationId"] = "HKLAKI";
 axios.defaults.headers.common["Promoir-Roomer-Hotel-Identifier"] = "2b72a454";
+
 router.get("/", async (req, res, next) => {
 	try {
-		const response = await axios.get(
-			"https://api.roomercloud.net:8080/services//hotelinfo/ROOM"
-		);
-
-		res.json(response.data);
+		const response = await axios.get("/services//hotelinfo/ROOM", {
+			headers: { Accept: "application/x-microsoft-dataset" }
+		});
+		const document = new xmldoc.XmlDocument(response.data);
+		let data = document.children[3].children.map(room => room.attr);
+		data.splice(0, 1);
+		res.json(data);
 	} catch (error) {
 		console.error(error.message);
 		res.status(500).json({ msg: error });
