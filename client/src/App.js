75810ff86f5f9070f8787ab0e88a8f748f924926
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-12-05 14:52:23 +0000

    start theming

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
+import "./styles/index.scss";
+import Navbar from "./components/layout/Navbar";
 
-const App = () => {
-	return <div>hi</div>;
-};
+const App = () => <Navbar />;
 
 export default App;
