d1fac68d7ea0551870e2b4a8f9ffe0cc410afa90
BenedyktT <benedykt.tyminski@gmail.com>
2019-12-12 14:44:38 +0000

    availability estimation req

--- routes/api/roomstatus.js
+++ routes/api/roomstatus.js
@@ -14,19 +14,95 @@ otplib.totp.options = {
   encoding: "hex"
 };
 
-router.get("/:date", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
+  const body = {
+    PaymentEstimateRequest: {
+      BookingRequestType: {
+        NewBooking: {
+          NewBooking: {
+            BookingType: {
+              guest: {
+                GuestType: {
+                  id: "000000000",
+                  firstName: "Benedykt",
+                  lastName: "Tyminski",
+                  gender: { GenderType: "MALE" },
+                  title: "Mr",
+                  dateOfBirth: "1991-02-26",
+                  guestAgeCategory: { GuestAgeCategoryType: "ADULT" },
+                  address: {
+                    AddressType: {
+                      houseNumber: "2424",
+                      extension: "2121",
+                      postOfficeBoxNumber: "1234",
+                      street: "test",
+                      postalCode: "1234",
+                      postalDistrict: "test",
+                      municipality: "test",
+                      county: "test",
+                      state: "test",
+                      country: "Iceland"
+                    }
+                  }
+                  /* contactDetails: { ContactDetailsType: {} } */
+                }
+              },
+              BaseRoomRequest: {
+                guest: "IDREF",
+                ratePlan: {},
+                roomType: "STD",
+                arrivalDateTime: "",
+                departureDateTime: ""
+              }
+            }
+          },
+          channelCode: "",
+          channelManagerCode: ""
+        }
+      }
+    }
+  };
   try {
     const token = otplib.totp.generate(atob(secret));
-    const response = await axios.get(
-      `roomer/openAPI/REST/inventories/rooms?statusDate=${req.params.date}`,
+    const response = await axios.post(
+      `roomer/openAPI/REST/estimates/booking`,
+      JSON.stringify(body),
       /* `roomer/openAPI/REST/bookings/roomassignments?roomNumber=${req.params.room_number}`, */
       {
         headers: {
           Accept: "application/json",
+          "Content-Type": "application/json",
           "Promoir-Roomer-Hotel-Secret": token
         }
       }
     );
+
+    const json = {
+      BookingRequestType: {
+        NewBooking: {
+          BookingType: {
+            guest: {
+              GuestType: {
+                id: "Stadgreitt",
+                firstName: "John",
+                address: {
+                  street: "Unknown",
+                  country: "IS"
+                }
+              }
+            },
+            BaseRoomRequest: {
+              guest: "Stadgreitt",
+              roomType: "STD",
+              arrivalDateTime: "2019-12-15T09:00:00",
+              departureDateTime: "2019-12-16T09:00:00"
+            }
+          }
+        }
+      }
+    };
+
+    req.body = json;
     /* 	const findNames = response.data.booking.map(guest =>
 			guest.baseRoomRequest.map(g => ({
 				name: g.value.guest.map(z => z.value.firstName).join(),
@@ -36,7 +112,7 @@ router.get("/:date", async (req, res, next) => {
 		
 
 		res.json(findNames[0]); */
-    res.json(response.data);
+    res.json(res.data);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ msg: error });
