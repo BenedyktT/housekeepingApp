13328cc2f063b99f2c9240f35aef704afe7b01bc
BenedyktT <benedykt.tyminski@gmail.com>
2019-12-13 23:22:10 +0000

    added sorting by room hallway

--- client/src/components/rooms/FiltersBar.js
+++ client/src/components/rooms/FiltersBar.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { ReactComponent as SortDown } from "../../img/sort-down-solid.svg";
+import { connect } from "react-redux";
+import {
+  showUnclean,
+  showAll,
+  selectHallway
+} from "../../actions/filterAction";
 
-const FiltersBar = () => {
-	return (
-		<div className="container ">
-			<div className=" rounded shadow filter-bar">
-				<span className="hamburger hamburger--small"></span>
-			</div>
-		</div>
-	);
+const FiltersBar = ({ showAll, showUnclean, selectHallway }) => {
+  const [isHamburgerActive, toggleHamburgerActive] = useState(false);
+
+  return (
+    <div className="container ">
+      <div className=" rounded shadow filter-bar">
+        <select
+          defaultValue="all"
+          onChange={e => {
+            e.target.value === "all" ? showAll() : showUnclean();
+          }}
+          className="sm-text-1"
+          name="filterOptions"
+        >
+          <option value="all">Show all</option>
+          <option value="unclean">Show unclean</option>
+        </select>
+        <select
+          defaultValue="unclean"
+          onChange={e => {
+            selectHallway(e.target.value);
+          }}
+          className="sm-text-1"
+          name="filterOptions"
+        >
+          <option value="all">All</option>
+          <option value="100">100</option>
+          <option value="200">200</option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
-export default FiltersBar;
+export default connect(null, { showAll, showUnclean, selectHallway })(
+  FiltersBar
+);
+
+/* 
+  <button
+          onClick={() => {
+            toggleHamburgerActive(!isHamburgerActive);
+          }}
+		></button>
+		 */
--- client/src/selectors/visibleRooms.js
+++ client/src/selectors/visibleRooms.js
@@ -1,10 +1,23 @@
-export default (rooms, { filterRooms }) => {
-	return rooms.filter(({ number, cleanStatus, vacancy, roomStatus }) => {
-		if (filterRooms === "SHOW_UNCLEAN") {
-			return cleanStatus === "Not Clean";
-		}
-		if (filterRooms === "SHOW_ALL") {
-			return true;
-		}
-	});
+export default (rooms, { filterCleanRoomsMethod, setVisibleHallway }) => {
+  console.log(setVisibleHallway);
+  return rooms
+    .filter(({ number, cleanStatus, vacancy, roomStatus }) => {
+      if (filterCleanRoomsMethod === "SHOW_UNCLEAN") {
+        return cleanStatus === "Not Clean";
+      }
+      if (filterCleanRoomsMethod === "SHOW_ALL") {
+        return true;
+      }
+    })
+    .filter(({ number }) => {
+      if (setVisibleHallway === "all") {
+        return true;
+      }
+      if (setVisibleHallway === "100") {
+        return parseInt(number) <= 116;
+      }
+      if (setVisibleHallway === "200") {
+        return parseInt(number) > 116 && parseInt(number) <= 224;
+      }
+    });
 };
