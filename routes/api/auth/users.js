dba5556f287f98f07e6ab4b2488ea62c5aad79db
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2019-12-26 14:19:50 +0000

    user login user register route

--- routes/api/auth/users.js
+++ routes/api/auth/users.js
@@ -2,27 +2,50 @@ const express = require("express");
 const User = require("../../../models/User");
 const config = require("config");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
-router.post("/", async (req, res) => {
-  const { name, email, password } = req.body;
-  let user = await User.findOne({ email });
+//register new user
 
-  try {
-    if (user) {
-      res.status(400).json({ errors: [{ msg: "User already exist" }] });
-    }
+router.post("/register", async (req, res) => {
+	const { name, email, password } = req.body;
+	let user = await User.findOne({ email });
 
-    user = new User({
-      name,
-      email,
-      password
-    });
-    await user.save();
-    res.json(user);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json("Internal Server Error");
-  }
+	try {
+		if (user) {
+			res.status(400).json({ errors: [{ msg: "User already exist" }] });
+		}
+
+		user = new User({
+			name,
+			email,
+			password
+		});
+		const salt = await bcrypt.genSalt(10);
+		user.password = await bcrypt.hash(password, salt);
+		await user.save();
+		res.json(user);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json("Internal Server Error");
+	}
+});
+
+router.post("/login", async (req, res) => {
+	const { name, password } = req.body;
+	try {
+		let user = await User.findOne({ name });
+		if (!user) {
+			return res.json("Didn't find the user");
+		}
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return res.json("Invalid Credentials");
+		}
+		res.json(user);
+	} catch (error) {
+		console.error(error);
+		res.json("internal server error");
+	}
 });
 
 module.exports = router;
