17be2f788802ff38bf3104e230c2c3f5f3beaa7c
BenedyktT <benedykt.tyminski@gmail.com>
2019-12-27 09:45:46 +0000

    working at oauth2 with passport

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,29 +1,35 @@
 import React from "react";
-
+import axios from "axios";
 const Landing = () => {
+  const onClick = async e => {
+    "google clicked";
+    const res = await axios.get("/auth/google");
+    console.log(res.data);
+  };
   return (
     <div className="container border ">
       <form action="" className="form">
         <label htmlFor="name">Name: </label>
         <input
-          className="padding-small rounded margin-small-x "
+          className="padding-small  margin-small-x "
           name="name"
           type="text"
         />
         <label htmlFor="email">Email: </label>
         <input
-          className="padding-small rounded margin-small-x "
+          className="padding-small  margin-small-x "
           name="email"
           type="text"
         />
         <label htmlFor="password">Password: </label>
         <input
-          className="padding-small rounded margin-small-x "
+          className="padding-small  margin-small-x "
           name="password"
           type="text"
         />
         <input className="padding-small margin-y" type="submit" value="Login" />
       </form>
+      <button onClick={onClick}>Google Login</button>
     </div>
   );
 };
