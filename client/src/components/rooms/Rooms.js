80be01597ee0e45c2326c449339c25d58312538d
BenedyktT <benedykt.tyminski@gmail.com>
2020-01-04 13:19:32 +0000

    fixed no show bug

--- client/src/components/rooms/Rooms.js
+++ client/src/components/rooms/Rooms.js
@@ -7,78 +7,79 @@ import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const Rooms = ({ rooms, loadRooms, setReportDate }) => {
-	const initLoadRooms = useCallback(
-		report => {
-			loadRooms(report);
-		},
-		[loadRooms]
-	);
-	useEffect(() => {
-		initLoadRooms(setReportDate);
-	}, [initLoadRooms]);
-	const render = () => {
-		return rooms.map(({ number, cleanStatus, vacancy, roomStatus }) => {
-			return (
-				<div
-					key={number}
-					className={classnames(
-						"room",
-						{
-							"gradient-blue":
-								vacancy === "Vacant" ||
-								vacancy === "Clean" ||
-								vacancy === "Not Clean"
-						},
-						{ "gradient-pink": vacancy === "Occupied" },
-						{ "gradient-green": vacancy === "Stayover" },
-						{ "gradient-dark": vacancy === "Out of Order" }
-					)}
-				>
-					<div className="room__col--left">
-						<h4 className="bold">Room {number}</h4>
-					</div>
-					<span className="hor-line"></span>
-					<div className="room__col--right">
-						<p className="occupancy small">{vacancy}</p>
-						<p className="reservation">{roomStatus}</p>
-						<p
-							className={
-								cleanStatus === "Clean"
-									? "success clean small"
-									: "danger clean small"
-							}
-						>
-							{cleanStatus}
-						</p>
-						<p className="note small"></p>
-					</div>
-				</div>
-			);
-		});
-	};
-	return (
-		<div className="container">
-			{rooms.length ? (
-				render()
-			) : (
-				<div className="loader-container">
-					<Loader
-						type="CradleLoader"
-						color="#00BFFF"
-						height={50}
-						width={50}
-						timeout={3000} //3 secs
-					/>
-				</div>
-			)}
-		</div>
-	);
+  const initLoadRooms = useCallback(
+    report => {
+      loadRooms(report);
+    },
+    [loadRooms]
+  );
+  useEffect(() => {
+    initLoadRooms(setReportDate);
+  }, [initLoadRooms]);
+  const render = () => {
+    return rooms.map(({ number, cleanStatus, vacancy, roomStatus }) => {
+      return (
+        <div
+          key={number}
+          className={classnames(
+            "room",
+            {
+              "gradient-blue":
+                vacancy === "Vacant" ||
+                vacancy === "Clean" ||
+                vacancy === "Not Clean" ||
+                vacancy == null
+            },
+            { "gradient-pink": vacancy === "Occupied" },
+            { "gradient-green": vacancy === "Stayover" },
+            { "gradient-dark": vacancy === "Out of Order" }
+          )}
+        >
+          <div className="room__col--left">
+            <h4 className="bold">Room {number}</h4>
+          </div>
+          <span className="hor-line"></span>
+          <div className="room__col--right">
+            <p className="occupancy small">{vacancy}</p>
+            <p className="reservation">{roomStatus}</p>
+            <p
+              className={
+                cleanStatus === "Clean"
+                  ? "success clean small"
+                  : "danger clean small"
+              }
+            >
+              {cleanStatus}
+            </p>
+            <p className="note small"></p>
+          </div>
+        </div>
+      );
+    });
+  };
+  return (
+    <div className="container">
+      {rooms.length ? (
+        render()
+      ) : (
+        <div className="loader-container">
+          <Loader
+            type="CradleLoader"
+            color="#00BFFF"
+            height={50}
+            width={50}
+            timeout={3000} //3 secs
+          />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default connect(
-	state => ({
-		rooms: visibleRooms(state.roomReducer.roomsReport, state.filterReducer),
-		setReportDate: state.filterReducer.getCurrentCalendarValue
-	}),
-	{ loadRooms }
+  state => ({
+    rooms: visibleRooms(state.roomReducer.roomsReport, state.filterReducer),
+    setReportDate: state.filterReducer.getCurrentCalendarValue
+  }),
+  { loadRooms }
 )(Rooms);
