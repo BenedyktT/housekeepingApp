640031a51624d03e793fffb213c069a1ef49b892
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-08 21:30:07 +0000

    implemented google sheets

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,57 +1,56 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import avatar from "../../img/ayo-ogunseinde-THIs-cpyebg-unsplash.jpg";
 import { connect } from "react-redux";
 import classnames from "classnames";
 import { toggleNavbar } from "../../actions/layoutAction";
 
 const Navbar = ({ isAuthenticated, user, toggleNavbar, isNavbarOpen }) => {
-  return (
-    <nav className="nav">
-      <div className="avatar">
-        {isAuthenticated ? (
-          <Fragment>
-            <img src={avatar} alt="avatar" className="avatar__image" />
-            <h3 className="avatar__greet text-white ml-1 sm-text-1">
-              Hi,{" "}
-              {user
-                ? user.name.charAt(0).toUpperCase() + user.name.slice(1)
-                : "dear"}
-            </h3>
-          </Fragment>
-        ) : (
-          <Fragment>
-            <h3 className="avatar__greet text-white ml-1 sm-text-1">
-              Housekeeping App
-            </h3>
-          </Fragment>
-        )}
-      </div>
-      <label className={classnames("hamburger-container")}>
-        <div
-          className={classnames("hamburger", {
-            active: isNavbarOpen
-          })}
-        ></div>
-        <input
-          type="checkbox"
-          onChange={() => {
-            console.log(isNavbarOpen);
-            toggleNavbar();
-          }}
-          className="hamburger__checkbox"
-          name="hamburger"
-          id="hamburger"
-        />
-      </label>
-    </nav>
-  );
+	return (
+		<nav className="nav">
+			<div className="avatar">
+				{isAuthenticated ? (
+					<Fragment>
+						<img src={avatar} alt="avatar" className="avatar__image" />
+						<h3 className="avatar__greet text-white ml-1 sm-text-1">
+							Hi,{" "}
+							{user
+								? user.name.charAt(0).toUpperCase() + user.name.slice(1)
+								: "dear"}
+						</h3>
+					</Fragment>
+				) : (
+					<Fragment>
+						<h3 className="avatar__greet text-white ml-1 sm-text-1">
+							Housekeeping App
+						</h3>
+					</Fragment>
+				)}
+			</div>
+			<label className={classnames("hamburger-container")}>
+				<div
+					className={classnames("hamburger", {
+						active: isNavbarOpen
+					})}
+				></div>
+				<input
+					type="checkbox"
+					onChange={() => {
+						toggleNavbar();
+					}}
+					className="hamburger__checkbox"
+					name="hamburger"
+					id="hamburger"
+				/>
+			</label>
+		</nav>
+	);
 };
 
 export default connect(
-  state => ({
-    isAuthenticated: state.authReducer.isAuthenticated,
-    user: state.authReducer.user,
-    isNavbarOpen: state.layoutReducer.isNavbarOpen
-  }),
-  { toggleNavbar }
+	state => ({
+		isAuthenticated: state.authReducer.isAuthenticated,
+		user: state.authReducer.user,
+		isNavbarOpen: state.layoutReducer.isNavbarOpen
+	}),
+	{ toggleNavbar }
 )(Navbar);
