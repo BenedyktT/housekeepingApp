d5fd05c749f08062fca63c4fae0e19120f42f3f5
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-12 23:25:28 +0000

    security vulnerabilities

--- routes/api/auth/middleware.js
+++ routes/api/auth/middleware.js
@@ -2,18 +2,18 @@ let jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = (req, res, next) => {
-  let token = req.headers["x-access-token"] || req.headers["authorization"];
+	let token = req.headers["x-access-token"] || req.headers["authorization"];
 
-  if (token) {
-    jwt.verify(token, config.get("secretID"), (err, decoded) => {
-      if (err) {
-        res.status(403).json({ errors: [{ msg: "Token is not valid" }] });
-      } else {
-        req.user = decoded.id;
-        next();
-      }
-    });
-  } else {
-    res.status(403).json({ errors: [{ msg: "Token does not exist" }] });
-  }
+	if (token) {
+		jwt.verify(token, process.env.API_secretID, (err, decoded) => {
+			if (err) {
+				res.status(403).json({ errors: [{ msg: "Token is not valid" }] });
+			} else {
+				req.user = decoded.id;
+				next();
+			}
+		});
+	} else {
+		res.status(403).json({ errors: [{ msg: "Token does not exist" }] });
+	}
 };
