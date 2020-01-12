2d5c7e42c06c490adea16d08dbf669b18996fd2a
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-12 17:14:06 +0000

    init deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/config");
+const path = require("path");
 
 connectDB();
 app.use(express.json({ extended: false }));
@@ -10,7 +11,14 @@ app.use("/cleanstatus", require("./routes/api/cleanstatus"));
 app.use("/reservation", require("./routes/api/reservationstatus"));
 app.use("/roomstatus", require("./routes/api/roomstatus"));
 app.use("/roomsetup", require("./routes/api/roomsetup"));
-app.get("/", (req, res) => res.send("API running"));
+
+//serve stati assets in prod
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server start on port ${PORT}`));
