706bb65a3339078f3968d496f4f533a013941a39
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-13 19:22:36 +0000

    fixed git history

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/config");
+require("dotenv").config();
+
+connectDB();
+app.use(express.json({ extended: false }));
+app.use("/user", require("./routes/api/auth/users"));
+app.use("/cleanstatus", require("./routes/api/cleanstatus"));
+app.use("/reservation", require("./routes/api/reservationstatus"));
+app.use("/roomstatus", require("./routes/api/roomstatus"));
+app.use("/roomsetup", require("./routes/api/roomsetup"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server start on port ${PORT}`));
