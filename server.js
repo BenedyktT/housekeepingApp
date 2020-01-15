6455fc720f5926e7e47d0ddd574dcab27aa55265
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-15 20:29:53 +0000

    gzip files

--- server.js
+++ server.js
@@ -21,9 +21,6 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server start on port ${PORT}`));
