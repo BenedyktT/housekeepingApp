8137f875b0b9703e6ff4e1aee8bccf96700b0c5d
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-19 20:49:53 +0000

    push favicon

--- routes/api/availability.js
+++ routes/api/availability.js
@@ -3,30 +3,30 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-router.get("/:arrival/:departure/:hotel", async (req, res) => {
-  const arrival = req.params.arrival;
-  const departure = req.params.departure;
-  const hotel = req.params.hotel || "LAKI";
-  try {
-    const response = await axios.get(
-      `https://api.roomercloud.net/services/bookingapi/availability1?hotel=${hotel}&channelCode=HOT&channelManagerCode=OWN&arrivalDate=${arrival}&departureDate=${departure}`
-    );
-    const document = new xmldoc.XmlDocument(response.data);
+router.get("/", async (req, res) => {
+	const arrival = req.params.arrival;
+	const departure = req.params.departure;
+	const hotel = req.params.hotel || "LAKI";
+	try {
+		const response = await axios.get(
+			`https://api.roomercloud.net/services/bookingapi/availability1?hotel=KLAUS&channelCode=BDC&channelManagerCode=OWN&arrivalDate=2020-01-19&departureDate=2020-01-20`
+		);
+		const document = new xmldoc.XmlDocument(response.data);
 
-    const availability = document.children[1].children.map(child => {
-      return {
-        room: child.attr.availabilityBaseCode,
-        desc: child.attr.description,
-        rate: child.children[1].children[0].attr.rate,
-        availability: child.children[1].children[0].attr.availability
-      };
-    });
+		const availability = document.children[1].children.map(child => {
+			return {
+				room: child.attr.availabilityBaseCode,
+				desc: child.attr.description,
+				rate: child.children[1].children[0].attr.rate,
+				availability: child.children[1].children[0].attr.availability
+			};
+		});
 
-    res.json(availability);
-  } catch (error) {
-    console.error(error);
-    res.json(error);
-  }
+		res.json(availability);
+	} catch (error) {
+		console.error(error);
+		res.json(error);
+	}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -13,12 +13,11 @@ app.use("/roomsetup", require("./routes/api/roomsetup"));
 app.use("/availability", require("./routes/api/availability"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("/*", function(req, res) {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
 }
 const PORT = process.env.PORT || 5000;
 
