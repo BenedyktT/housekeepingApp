09417aa3c1d94e84e3de67a489c87ae4ef3a27c6
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-20 09:14:21 +0000

    prolonged access token expiration date

--- client/src/store.js
+++ client/src/store.js
@@ -1,13 +1,13 @@
 import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
 const initialState = {};
 const middleware = [thunkMiddleware];
 const store = createStore(
-  rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+	rootReducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
