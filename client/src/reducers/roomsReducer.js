6561817f5138e3a432829edaac581631a71ba299
BenedyktT <benedykt.tyminski@gmail.com>
2020-01-27 14:46:25 +0000

    added hacky walkaround to fix report bug

--- client/src/reducers/roomsReducer.js
+++ client/src/reducers/roomsReducer.js
@@ -1,163 +1,171 @@
 import { GET_ROOM_SETUP, GET_CLEAN_ROOMS, LOAD_ROOMS } from "../actions/types";
 
 const initialState = {
-	roomsReport: [],
-	loading: null
+  roomsReport: [],
+  loading: null
 };
 
 export default (state = initialState, action) => {
-	const { type, payload } = action;
-	switch (type) {
-		case LOAD_ROOMS:
-			return { ...state, loading: true };
-		case GET_ROOM_SETUP:
-			const { cleanStatus, roomNote } = payload;
-			const { currentRooms, nextRooms } = payload.roomStatus;
-			//merge object from two requests (same key)
-			console.log(payload.roomStatus);
-			const currentRoomSetup = currentRooms
-				.filter((e, index, array) => {
-					const prevItem = array[index - 1];
-					const nextItem = array[index + 1];
-					if (
-						(nextItem && e.Room === nextItem.Room) ||
-						(prevItem && e.Room === prevItem.Room)
-					) {
-						return e.Room !== "VEIT" && e.roomState !== "N_A";
-					} else return e.Room !== "VEIT";
-				})
-				.map(occupancyRoom => {
-					const { Room } = occupancyRoom;
-					return Object.assign(
-						...cleanStatus.filter(el => el.Room === Room),
-						occupancyRoom
-					);
-				});
+  const { type, payload } = action;
+  switch (type) {
+    case LOAD_ROOMS:
+      return { ...state, loading: true };
+    case GET_ROOM_SETUP:
+      const { cleanStatus, roomNote } = payload;
+      const { currentRooms, nextRooms } = payload.roomStatus;
+      //merge object from two requests (same key)
+      console.log(payload.roomStatus);
+      const currentRoomSetup = currentRooms
+        .filter((e, index, array) => {
+          const prevItem = array[index - 1];
+          const nextItem = array[index + 1];
+          if (
+            (nextItem && e.Room === nextItem.Room) ||
+            (prevItem && e.Room === prevItem.Room)
+          ) {
+            return e.Room !== "VEIT" && e.roomState !== "N_A";
+          } else return e.Room !== "VEIT";
+        })
+        .map(occupancyRoom => {
+          const { Room } = occupancyRoom;
+          return Object.assign(
+            ...cleanStatus.filter(el => el.Room === Room),
+            occupancyRoom
+          );
+        });
 
-			//merge previous object with next day roomstatus report object
-			const roomSetup = currentRoomSetup.map(currentRoom => {
-				const { Room } = currentRoom;
-				const nextRoomsArr = nextRooms.map(room => ({
-					Room: room.Room,
-					nextRooms: room.roomState
-				}));
-				return Object.assign(
-					currentRoom,
-					...nextRoomsArr.filter(el => el.Room === Room)
-				);
-			});
-			const noshow = "NO_SHOW";
-			const co = "CHECKED_OUT";
-			const na = "N_A";
-			const avail = "AVAILABLE";
-			const arr = "ARRIVED";
-			const block = "BLOCKED";
-			const narr = "NOT_ARRIVED";
-			let roomsReport = roomSetup.map(
-				({ Room: number, Status: cleanStatus, roomState: c, nextRooms: n }) => {
-					let vacancy;
-					let roomStatus;
+      //merge previous object with next day roomstatus report object
+      const roomSetup = currentRoomSetup.map(currentRoom => {
+        const { Room } = currentRoom;
+        const nextRoomsArr = nextRooms.map(room => ({
+          Room: room.Room,
+          nextRooms: room.roomState
+        }));
+        return Object.assign(
+          currentRoom,
+          ...nextRoomsArr.filter(el => el.Room === Room)
+        );
+      });
+      const noshow = "NO_SHOW";
+      const vac = "VACANT";
+      const occ = "OCCUPIED";
+      const co = "CHECKED_OUT";
+      const na = "N_A";
+      const avail = "AVAILABLE";
+      const arr = "ARRIVED";
+      const block = "BLOCKED";
+      const narr = "NOT_ARRIVED";
+      let roomsReport = roomSetup.map(
+        ({ Room: number, Status: cleanStatus, roomState: c, nextRooms: n }) => {
+          let vacancy;
+          let roomStatus;
 
-					if (cleanStatus === "C") {
-						cleanStatus = "Clean";
-					}
-					if (cleanStatus === "U") {
-						cleanStatus = "Not Clean";
-					}
+          if (cleanStatus === "C") {
+            cleanStatus = "Clean";
+          }
+          if (cleanStatus === "U") {
+            cleanStatus = "Not Clean";
+          }
 
-					if (
-						(c === narr || c === avail || c === na || c === block) &&
-						(n === narr || n === avail || n === na || n === co || n === block)
-					) {
-						vacancy = cleanStatus;
-						roomStatus = cleanStatus;
-					}
-					if ((c === narr || c === avail || c === block) && n === arr) {
-						vacancy = "Occupied";
-						roomStatus = "Arriving";
-					}
+          if (
+            (c === narr || c === avail || c === na || c === block) &&
+            (n === narr || n === avail || n === na || n === co || n === block)
+          ) {
+            vacancy = cleanStatus;
+            roomStatus = cleanStatus;
+          }
+          if ((c === narr || c === avail || c === block) && n === arr) {
+            vacancy = "Occupied";
+            roomStatus = "Arriving";
+          }
 
-					if (
-						(c === block || c === na || c === avail || c === noshow) &&
-						n === narr
-					) {
-						vacancy = "Vacant";
-						roomStatus = "Arriving";
-					}
-					if (c === co && n === narr) {
-						vacancy = "Vacant";
-						roomStatus = "Departure+Arriving";
-					}
-					if ((c === arr || c === na) && n === arr) {
-						vacancy = "Stayover";
-						roomStatus = "Stayover";
-					}
-					if (c === co && n === arr) {
-						vacancy = "Vacant";
-						roomStatus = "Departure+Arriving";
-					}
-					if (c === arr && n === narr) {
-						vacancy = "Occupied";
-						roomStatus = "Departure+Arriving";
-					}
-					if (
-						(c === co || c === block || c === narr) &&
-						(n === avail || n === co || n === na)
-					) {
-						vacancy = "Vacant";
-						roomStatus = "Departure";
-					}
-					if (c === arr && (n === block || n === na || n === avail)) {
-						vacancy = "Occupied";
-						roomStatus = "Departure";
-					}
+          if (
+            (c === block || c === na || c === avail || c === noshow) &&
+            n === narr
+          ) {
+            vacancy = "Vacant";
+            roomStatus = "Arriving";
+          }
+          if (c === co && n === narr) {
+            vacancy = "Vacant";
+            roomStatus = "Departure+Arriving";
+          }
+          if ((c === arr || c === na) && n === arr) {
+            vacancy = "Stayover";
+            roomStatus = "Stayover";
+          }
+          if (c === co && n === arr) {
+            vacancy = "Vacant";
+            roomStatus = "Departure+Arriving";
+          }
+          if (c === arr && n === narr) {
+            vacancy = "Occupied";
+            roomStatus = "Departure+Arriving";
+          }
+          if (
+            (c === co || c === block || c === narr) &&
+            (n === avail || n === co || n === na)
+          ) {
+            vacancy = "Vacant";
+            roomStatus = "Departure";
+          }
+          if (c === arr && (n === block || n === na || n === avail)) {
+            vacancy = "Occupied";
+            roomStatus = "Departure";
+          }
 
-					if ((c === narr || c === avail || c === block) && n === block) {
-						vacancy = "Out of Order";
-						roomStatus = "Out of Order";
-					}
-					if (c === co && n === block) {
-						vacancy = "Vacant";
-						roomStatus = "Departure";
-					}
+          if ((c === narr || c === avail || c === block) && n === block) {
+            vacancy = "Out of Order";
+            roomStatus = "Out of Order";
+          }
+          if (c === co && n === block) {
+            vacancy = "Vacant";
+            roomStatus = "Departure";
+          }
+          if (c === vac) {
+            vacancy = "Vacant";
+          }
+          if (c === occ) {
+            vacancy = "Occupied";
+          }
 
-					return {
-						number,
-						cleanStatus,
-						vacancy,
-						roomStatus
-					};
-				}
-			);
-			roomsReport = roomsReport.map(room => {
-				const { number } = room;
-				return Object.assign(
-					...roomNote.filter(el => {
-						if (el.roomNote) {
-							return el.number === number;
-						} else {
-							return null;
-						}
-					}),
-					room
-				);
-			});
+          return {
+            number,
+            cleanStatus,
+            vacancy,
+            roomStatus
+          };
+        }
+      );
+      roomsReport = roomsReport.map(room => {
+        const { number } = room;
+        return Object.assign(
+          ...roomNote.filter(el => {
+            if (el.roomNote) {
+              return el.number === number;
+            } else {
+              return null;
+            }
+          }),
+          room
+        );
+      });
 
-			return { ...state, roomsReport, loading: false };
+      return { ...state, roomsReport, loading: false };
 
-		case GET_CLEAN_ROOMS:
-			const newState = state.roomsReport.filter(e => {
-				return payload.map(cleanRooms => {
-					if (cleanRooms.number !== parseInt(e.number)) return null;
-					e.cleanStatus = "Clean";
-					e.cleanedBy = cleanRooms.username;
-					return null;
-				});
-			});
+    case GET_CLEAN_ROOMS:
+      const newState = state.roomsReport.filter(e => {
+        return payload.map(cleanRooms => {
+          if (cleanRooms.number !== parseInt(e.number)) return null;
+          e.cleanStatus = "Clean";
+          e.cleanedBy = cleanRooms.username;
+          return null;
+        });
+      });
 
-			return { ...state, roomsReport: [...newState], loading: false };
+      return { ...state, roomsReport: [...newState], loading: false };
 
-		default:
-			return state;
-	}
+    default:
+      return state;
+  }
 };
--- routes/api/reservationstatus.js
+++ routes/api/reservationstatus.js
@@ -9,42 +9,69 @@ axios.defaults.baseURL = "https://api.roomercloud.net";
 axios.defaults.headers.common["Promoir-Roomer-Hotel-ApplicationId"] = "HKLAKI";
 axios.defaults.headers.common["Promoir-Roomer-Hotel-Identifier"] = "2b72a454";
 otplib.totp.options = {
-	digits: 8,
-	algorithm: "sha256",
-	encoding: "hex"
+  digits: 8,
+  algorithm: "sha256",
+  encoding: "hex"
 };
 router.get("/:date", async (req, res, next) => {
-	try {
-		const token = otplib.totp.generate(atob(secret));
+  try {
+    const token = otplib.totp.generate(atob(secret));
 
-		const response = await axios.get(
-			`roomer/openAPI/REST/inventories/rooms?statusDate=${req.params.date}`,
-			{
-				headers: {
-					"Promoir-Roomer-Hotel-Secret": token
-				}
-			}
-		);
-		let rooms = response.data.roomStatusList.roomStatus;
-		rooms = rooms.map(room =>
-			(({ roomName, roomState }) => ({ Room: roomName, roomState }))(room)
-		);
+    const response = await axios.get(
+      `roomer/openAPI/REST/inventories/rooms?statusDate=${req.params.date}`,
+      {
+        headers: {
+          "Promoir-Roomer-Hotel-Secret": token
+        }
+      }
+    );
+    let rooms = response.data.roomStatusList.roomStatus;
+    rooms = rooms.map(room =>
+      (({ roomName, roomState }) => ({ Room: roomName, roomState }))(room)
+    );
+    const url = rooms
+      .filter(e => {
+        return e.roomState === "N_A";
+      })
+      .map(x => ({
+        url: `roomer/openAPI/REST/bookings/roomassignments?roomNumber=${x.Room}`,
+        Room: x.Room
+      }));
 
-		res.json(rooms);
-	} catch (error) {
-		console.error(error.message);
-		res.status(500).json({ msg: error });
-	}
+    const roomsVacant = url.map(async promise => {
+      try {
+        await axios({
+          method: "GET",
+          url: promise.url,
+          headers: {
+            "Promoir-Roomer-Hotel-Secret": token
+          }
+        });
+        return {
+          Room: promise.Room,
+          roomState: "OCCUPIED"
+        };
+      } catch (error) {
+        return { Room: promise.Room, roomState: "VACANT" };
+      }
+    });
+    const results = await Promise.all(roomsVacant);
+    const combined = [...rooms, ...results];
+    rooms = combined
+      .reduce((acc, curr) => {
+        const x = acc.find(item => item.Room === curr.Room);
+        if (!x) {
+          return curr.roomState === "N_A" ? acc : acc.concat([curr]);
+        } else {
+          return acc;
+        }
+      }, [])
+      .sort((a, b) => a.Room - b.Room);
+    res.json(rooms);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: error });
+  }
 });
 
 module.exports = router;
-
-/* 
-const response = await axios.get(
-  `roomer/openAPI/REST//bookings/roomassignments?roomNumber=${e.Room}`,
-  {
-    headers: {
-      "Promoir-Roomer-Hotel-Secret": token
-    }
-  }
-); */
