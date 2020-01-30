dd639dfdff36dc6063480f96cc3382ae3f980a09
Lobbý Hótel Laki 1 <benedykt.tyminski@gmail.com>
2020-01-30 21:17:11 +0000

    set a nap in wakeUpDyno

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const connectDB = require("./config/config");
 require("dotenv").config();
 const wakeUpDyno = require("./wakeUpDyno");
+const session = require("express-session");
+const moment = require("moment");
 
 connectDB();
 app.use(express.json({ extended: false }));
@@ -14,15 +16,21 @@ app.use("/roomsetup", require("./routes/api/roomsetup"));
 app.use("/availability", require("./routes/api/availability"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-  app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-  });
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("/*", function(req, res) {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
 }
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  wakeUpDyno("http://lakihk.herokuapp.com/");
-  console.log(`server start on port ${PORT}`);
+	wakeUpDyno({
+		url: "http://lakihk.herokuapp.com/", // url string
+		interval: 60000 * 25, // interval in milliseconds (1 minute in this example)
+		startNap: [19, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
+		endNap: [06, 59, 59, 999] // time to wake up again, in UTC (09:59:59.999 in this example)
+	}).start();
+
+	console.log(`server start on port ${PORT}`);
 });
--- wakeUpDyno.js
+++ wakeUpDyno.js
@@ -1,31 +1,79 @@
 const axios = require("axios");
 
-const wakeUpDyno = (url, interval = 25, callback) => {
-  const milliseconds = interval * 60000;
-  setTimeout(() => {
-    try {
-      console.log(`setTimeout called.`);
-      // HTTP GET request to the dyno's url
-      axios
-        .get(url)
-        .then(() => console.log(`Fetching ${url}.`))
-        .catch(err => console.log("Error"));
-    } catch (err) {
-      // catch fetch errors
-      console.log(`Error fetching ${url}: ${err.message} 
-            Will try again in ${interval} minutes...`);
-    } finally {
-      try {
-        callback(); // execute callback, if passed
-      } catch (e) {
-        // catch callback error
-        callback ? console.log("Callback failed: ", e.message) : null;
-      } finally {
-        // do it all again
-        return wakeUpDyno(url, interval, callback);
-      }
-    }
-  }, milliseconds);
+/*
+If current time falls between startTime and endTime, returns length startTime and endTime are arrays of numbers representing the time of day. They follow this pattern: [Hours, Minutes, Seconds, Milliseconds]. If endTime is less than startTime time, endTime will be assumed to be on the following day. 
+If current time is not between startTime and endTime, will return false */
+const timeToNap = (startTime, endTime, now) => {
+	const todayArray = [
+		now.getUTCFullYear(),
+		now.getUTCMonth(),
+		now.getUTCDate()
+	];
+	const start = new Date(Date.UTC(...todayArray, ...startTime));
+	const end = new Date(Date.UTC(...todayArray, ...endTime));
+	const finish = start < end ? end : end.setDate(end.getDate() + 1);
+	if (now >= start && now <= finish) {
+		console.log("it is naptime");
+		return finish - now;
+	}
+	return false;
+};
+
+const wakeUpDyno = options => {
+	let url;
+	if (typeof options === "string") {
+		url = options;
+	} else {
+		url = options.url;
+	}
+	let interval = options.interval || 1.5e6;
+	let startNap = options.startNap || [0, 0, 0, 0];
+	let endNap = options.endNap || [0, 0, 0, 1];
+	const minutes = (interval / 60000).toFixed(2);
+	const minuteString = `${minutes} ${
+		interval / 60000 === 1 ? "minute" : "minutes"
+	}`;
+	console.log(`wokeDyno called with an interval of ${minuteString}.`);
+	const runTimer = timerInterval => {
+		const timeoutFn = () => {
+			timerInterval = interval; // reset to original interval, after nap
+			const naptime = timeToNap(startNap, endNap, new Date(Date.now())); // if nap, will return length of nap in ms
+			console.log(naptime);
+			if (naptime) {
+				const napString = `${(naptime / 60000).toFixed(2)} ${
+					Math.floor(minutes) > 1 ? "minutes" : "minute"
+				}`;
+				console.log(`It's naptime! Napping for ${napString}...`);
+				return runTimer(naptime); // take a nap
+			}
+			if (!naptime) {
+				console.log("it is not naptime");
+			}
+			axios
+				.get(url)
+				.then(() =>
+					console.log(
+						`Fetching ${url}. Dyno is woke. \nNext fetch request in ${minuteString}...`
+					)
+				)
+				.catch(error => console.log(`Error fetching ${url}: ${error.message}`));
+			clearTimeout(timeoutId);
+			return runTimer(timerInterval); // run timer with original interval
+		};
+		const timeoutId = setTimeout(timeoutFn, timerInterval);
+		return timeoutId;
+	};
+	const start = () => {
+		try {
+			return runTimer(interval);
+		} catch (error) {
+			console.log("setTimeout error:", error.message);
+		}
+	};
+	return {
+		start,
+		runTimer
+	};
 };
 
 module.exports = wakeUpDyno;
