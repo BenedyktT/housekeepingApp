ecf9f406c513ee8c96bf0929cf38547a768e8d8e
BenedyktT <benedykt.tyminski@gmail.com>
2020-02-07 22:25:35 +0000

    show room overview bugfix

--- client/src/actions/statisticActions.js
+++ client/src/actions/statisticActions.js
@@ -2,15 +2,16 @@ import { GET_STATISTIC } from "./types";
 import axios from "axios";
 
 export const getStatistic = ({ c, n }, outoforder) => async dispatch => {
-	try {
-		if (c && n) {
-			const response = await axios.get(`/availability/${c}/${n}`);
-			dispatch({
-				type: GET_STATISTIC,
-				payload: [...response.data, { room: "OOO", occupancy: outoforder }]
-			});
-		}
-	} catch (error) {
-		console.error(error.message);
-	}
+  console.log("this dispatched");
+  try {
+    if (c && n) {
+      const response = await axios.get(`/availability/${c}/${n}`);
+      dispatch({
+        type: GET_STATISTIC,
+        payload: [...response.data, { room: "OOO", occupancy: outoforder }]
+      });
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
--- client/src/components/rooms/Overview.js
+++ client/src/components/rooms/Overview.js
@@ -5,32 +5,32 @@ import moment from "moment";
 import { Fragment } from "react";
 
 const Overview = ({ getStatistic, calendarDate, rooms, totalRooms }) => {
-	useEffect(() => {
-		const outOfOrder = rooms.filter(e => e.vacancy === "Out of Order").length;
-		if (calendarDate) {
-			getStatistic(calendarDate, outOfOrder);
-		}
-	}, []);
-	return (
-		<Fragment>
-			{!moment(calendarDate.n).isSameOrBefore(moment(), "day") && (
-				<div className="overview container">
-					<span>
-						Total: <i className="bold">{totalRooms} </i>
-						rooms
-					</span>
-				</div>
-			)}
-		</Fragment>
-	);
+  useEffect(() => {
+    const outOfOrder = rooms.filter(e => e.vacancy === "Out of Order").length;
+    if (calendarDate) {
+      getStatistic(calendarDate, outOfOrder);
+    }
+  }, [calendarDate]);
+  return (
+    <Fragment>
+      {!moment(calendarDate.n).isSameOrBefore(moment(), "day") && (
+        <div className="overview container">
+          <span>
+            Total: <i className="bold">{totalRooms} </i>
+            rooms
+          </span>
+        </div>
+      )}
+    </Fragment>
+  );
 };
 
 export default connect(
-	state => ({
-		totalRooms: state.statisticReducer.totalRooms,
-		calendarDate: state.filterReducer.getCurrentCalendarValue,
-		rooms: state.roomReducer.roomsReport,
-		loading: state.roomReducer.loading
-	}),
-	{ getStatistic }
+  state => ({
+    totalRooms: state.statisticReducer.totalRooms,
+    calendarDate: state.filterReducer.getCurrentCalendarValue,
+    rooms: state.roomReducer.roomsReport,
+    loading: state.roomReducer.loading
+  }),
+  { getStatistic }
 )(Overview);
