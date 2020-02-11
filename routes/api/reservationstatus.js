8720071e5e9e968e1e5ceaba6dc21de6b244c422
BenedyktT <benedykt.tyminski@gmail.com>
2020-02-11 14:13:34 +0000

    503 error from roomer quick fix@

--- routes/api/reservationstatus.js
+++ routes/api/reservationstatus.js
@@ -57,18 +57,68 @@ router.get("/:date", async (req, res, next) => {
     });
     const results = await Promise.all(roomsVacant);
     const combined = [...rooms, ...results];
-    rooms = combined
-      .reduce((acc, curr) => {
+    rooms = combined.reduce((acc, curr) => {
+      const x = acc.find(item => item.Room === curr.Room);
+      if (!x) {
+        return curr.roomState === "N_A" ? acc : acc.concat([curr]);
+      } else {
+        return acc;
+      }
+    }, []);
+    res.json(rooms);
+  } catch (error) {
+    if (error.response.status === 503) {
+      const token = otplib.totp.generate(atob(secret));
+      const response = await axios.get(
+        `roomer/openAPI/REST/inventories/rooms?statusDate=${req.params.date}`,
+        {
+          headers: {
+            "Promoir-Roomer-Hotel-Secret": token
+          }
+        }
+      );
+      let rooms = response.data.roomStatusList.roomStatus;
+      rooms = rooms.map(room =>
+        (({ roomName, roomState }) => ({ Room: roomName, roomState }))(room)
+      );
+      const url = rooms
+        .filter(e => {
+          return e.roomState === "N_A";
+        })
+        .map(x => ({
+          url: `roomer/openAPI/REST/bookings/roomassignments?roomNumber=${x.Room}`,
+          Room: x.Room
+        }));
+
+      const roomsVacant = url.map(async promise => {
+        try {
+          await axios({
+            method: "GET",
+            url: promise.url,
+            headers: {
+              "Promoir-Roomer-Hotel-Secret": token
+            }
+          });
+          return {
+            Room: promise.Room,
+            roomState: "OCCUPIED"
+          };
+        } catch (error) {
+          return { Room: promise.Room, roomState: "VACANT" };
+        }
+      });
+      const results = await Promise.all(roomsVacant);
+      const combined = [...rooms, ...results];
+      rooms = combined.reduce((acc, curr) => {
         const x = acc.find(item => item.Room === curr.Room);
         if (!x) {
           return curr.roomState === "N_A" ? acc : acc.concat([curr]);
         } else {
           return acc;
         }
-      }, [])
-      .sort((a, b) => a.Room - b.Room);
-    res.json(rooms);
-  } catch (error) {
+      }, []);
+      res.json(rooms);
+    }
     console.error(error.message);
     res.status(500).json({ msg: error });
   }
